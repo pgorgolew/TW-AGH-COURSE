41252399a2aefbde0cfeeedbb97ad6ca4069d5a8
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2022-10-28 00:53:56 +0200

    js homework init

---
+++ lab3-homework/js-homework/philosophers.js
@@ -0,0 +1,93 @@
+// Teoria Współbieżnośi, implementacja problemu 5 filozofów w node.js
+// Opis problemu: http://en.wikipedia.org/wiki/Dining_philosophers_problem
+//   https://pl.wikipedia.org/wiki/Problem_ucztuj%C4%85cych_filozof%C3%B3w
+// 1. Dokończ implementację funkcji podnoszenia widelca (Fork.acquire).
+// 2. Zaimplementuj "naiwny" algorytm (każdy filozof podnosi najpierw lewy, potem
+//    prawy widelec, itd.).
+// 3. Zaimplementuj rozwiązanie asymetryczne: filozofowie z nieparzystym numerem
+//    najpierw podnoszą widelec lewy, z parzystym -- prawy.
+// 4. Zaimplementuj rozwiązanie z kelnerem (według polskiej wersji strony)
+// 5. Zaimplementuj rozwiążanie z jednoczesnym podnoszeniem widelców:
+//    filozof albo podnosi jednocześnie oba widelce, albo żadnego.
+// 6. Uruchom eksperymenty dla różnej liczby filozofów i dla każdego wariantu
+//    implementacji zmierz średni czas oczekiwania każdego filozofa na dostęp
+//    do widelców. Wyniki przedstaw na wykresach.
+
+var Fork = function() {
+    this.state = 0;
+    return this;
+}
+
+Fork.prototype.acquire = function(cb) {
+    // zaimplementuj funkcję acquire, tak by korzystala z algorytmu BEB
+    // (http://pl.wikipedia.org/wiki/Binary_Exponential_Backoff), tzn:
+    // 1. przed pierwszą próbą podniesienia widelca Filozof odczekuje 1ms
+    // 2. gdy próba jest nieudana, zwiększa czas oczekiwania dwukrotnie
+    //    i ponawia próbę, itd.
+}
+
+Fork.prototype.release = function() {
+    this.state = 0;
+}
+
+var Philosopher = function(id, forks) {
+    this.id = id;
+    this.forks = forks;
+    this.f1 = id % forks.length;
+    this.f2 = (id+1) % forks.length;
+    return this;
+}
+
+Philosopher.prototype.startNaive = function(count) {
+    var forks = this.forks,
+        f1 = this.f1,
+        f2 = this.f2,
+        id = this.id,
+
+        // zaimplementuj rozwiązanie naiwne
+        // każdy filozof powinien 'count' razy wykonywać cykl
+        // podnoszenia widelców -- jedzenia -- zwalniania widelców
+        }
+
+Philosopher.prototype.startAsym = function(count) {
+    var forks = this.forks,
+        f1 = this.f1,
+        f2 = this.f2,
+        id = this.id,
+
+        // zaimplementuj rozwiązanie asymetryczne
+        // każdy filozof powinien 'count' razy wykonywać cykl
+        // podnoszenia widelców -- jedzenia -- zwalniania widelców
+        }
+
+Philosopher.prototype.startConductor = function(count) {
+    var forks = this.forks,
+        f1 = this.f1,
+        f2 = this.f2,
+        id = this.id,
+
+        // zaimplementuj rozwiązanie z kelnerem
+        // każdy filozof powinien 'count' razy wykonywać cykl
+        // podnoszenia widelców -- jedzenia -- zwalniania widelców
+        }
+
+
+// TODO: wersja z jednoczesnym podnoszeniem widelców
+// Algorytm BEB powinien obejmować podnoszenie obu widelców,
+// a nie każdego z osobna
+
+
+var N = 5;
+var forks = [];
+var philosophers = []
+for (var i = 0; i < N; i++) {
+    forks.push(new Fork());
+}
+
+for (var i = 0; i < N; i++) {
+    philosophers.push(new Philosopher(i, forks));
+}
+
+for (var i = 0; i < N; i++) {
+    philosophers[i].startNaive(10);
+}
